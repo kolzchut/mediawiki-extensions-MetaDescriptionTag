0f5bb2bc10de10ccd627f184a0223c3c9d0ad7e4
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2017-05-22 01:43:51 +0300

    Commit of the old 0.3 version of the extension

---
+++ MetaDescriptionTag.i18n.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Internationalisation file for extension MetaDescriptionTag.
+ */
+ 
+$messages = array();
+
+/** English (English)
+ * @author David M. Sledge
+ * @author Purodha
+ */
+$messages['en'] = array(
+	# general messages
+	'metadescriptiontag-desc'	=> 'Tag to inject meta description into page header.',
+	'metadescriptiontag-missing-content' => 'Error: &lt;metadesc&gt; tag must contain a &quot;content&quot; attribute.',
+);
+
+/** Message documentation (Message documentation)
+ * @author DrorSnir
+ */
+$messages['qqq'] = array(
+	'metadescriptiontag-desc' => '{{desc}}',
+	'metadescriptiontag-missing-content' => 'Error message for wrong usage of tag - missing content attribute',
+);
+
+/** Hebrew (עברית)
+ * @author DrorSnir
+ */
+$messages['he'] = array(
+	'metadescriptiontag-desc' => 'תגית המאפשרת לקבוע את תיאור העמוד עבור מנועי חיפוש (Meta description)',
+	'metadescriptiontag-missing-content' => 'שגיאה: תגית &lt;metadesc&gt; חייבת לכלול תכונת &lt;metadesc&gt;.',
+);
+
+
+
